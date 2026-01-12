2c5ad4db5fed889e8f8584fde1e5ea6931c3434b
Sulamyt Uderzo <sulamytuderzo@Macbook-Air-di-Sulamyt.local>
2026-01-12 20:51:33 +0100

    Update module navigation

--- src/module.vue
+++ src/module.vue
@@ -3,16 +3,14 @@
     <template #navigation>
       <v-list nav>
         <v-list-item 
-          :to="`/ia/base-conoscenza`"
-          exact
+          :to="`/content/Base_di_conoscenza`"
         >
           <v-list-item-icon><v-icon name="auto_stories" /></v-list-item-icon>
           <v-list-item-content>Base di conoscenza</v-list-item-content>
         </v-list-item>
         
         <v-list-item 
-          :to="`/ia/prompt`"
-          exact
+          :to="`/content/Prompt`"
         >
           <v-list-item-icon><v-icon name="code" /></v-list-item-icon>
           <v-list-item-content>Prompt</v-list-item-content>
@@ -20,7 +18,9 @@
       </v-list>
     </template>
 
-    <router-view />
+    <div class="empty">
+      <p>Seleziona una sezione dal menu laterale</p>
+    </div>
   </private-view>
 </template>
 
@@ -29,3 +29,13 @@ export default {
   name: 'IAModule'
 };
 </script>
+
+<style scoped>
+.empty {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  color: var(--foreground-subdued);
+}
+</style>
